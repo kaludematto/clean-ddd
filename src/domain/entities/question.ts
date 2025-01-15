4230eede27270621ef5c8e623f59b93f7fcd8685
Kalu <kalu.dematto@gmail.com>
2025-01-15 17:10:47 -0300

    mapping props

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -3,10 +3,13 @@ import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 
 interface QuestionProps {
+    authorId: UniqueEntityID
+    bestAnswerId?: UniqueEntityID
     title: string
     content: string
     slug: Slug
-    authorId: UniqueEntityID
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {
