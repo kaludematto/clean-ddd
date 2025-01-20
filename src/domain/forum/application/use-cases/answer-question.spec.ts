5400dcbb9c1681f68bdba5e22605d64a83b32395
kaludematto <kalu.dematto@gmail.com>
2025-01-20 12:13:06 -0300

    create question tests

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,9 +1,10 @@
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
 import { AnswerRepository } from '../repositories/answers-repository'
-import { Answer } from '../forum/enterprise/entities/answer'
+import { Answer } from '../../enterprise/entities/answer'
 
 const fakeAnswersRepository: AnswerRepository = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   create: async (answer: Answer) => {},
 }
 
---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest'
+import { QuestionsRepository } from '../repositories/question-repository'
+import { Question } from '../../enterprise/entities/question'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create: async (question: Question) => {},
+}
+test('create an answer', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'New Question',
+    content: 'Nova Resposta',
+  })
+  expect(question.id).toBeTruthy()
+})
