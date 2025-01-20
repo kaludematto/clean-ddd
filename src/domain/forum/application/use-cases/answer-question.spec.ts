ce36533c99bfb6bd953ecf92cd5c9ea0fe2f99e8
kaludematto <kalu.dematto@gmail.com>
2025-01-20 10:40:18 -0300

    refactor folders

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
 import { AnswerRepository } from '../repositories/answers-repository'
-import { Answer } from '../entities/answer'
+import { Answer } from '../forum/enterprise/entities/answer'
 
 const fakeAnswersRepository: AnswerRepository = {
   create: async (answer: Answer) => {},
