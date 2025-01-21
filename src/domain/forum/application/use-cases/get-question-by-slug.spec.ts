6c095c6806185c5df18ab5586fc2802c9a3fa4ad
kaludematto <kalu.dematto@gmail.com>
2025-01-20 22:59:29 -0300

    get question unit test

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,33 @@
+import { expect, it } from 'vitest'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { GetQuestionBySlugUseCase } from './get-question-by-slug'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Slug } from '../../enterprise/entities/value-objects/slug'
+import { Question } from '../../enterprise/entities/question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = Question.create({
+      title: 'Example Question',
+      slug: Slug.create('example-question'),
+      authorId: new UniqueEntityID(),
+      content: 'Example Content',
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+  })
+})
