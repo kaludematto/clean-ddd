7d9528cd9142862f050c407f6d50a0f50e0ddf8a
kaludematto <kalu.dematto@gmail.com>
2025-01-22 18:29:57 -0300

    update in memory repo

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -4,6 +4,15 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+    return question
+  }
+
   async create(question: Question) {
     this.items.push(question)
   }
@@ -15,5 +24,11 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
       return null
     }
     return question
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
