5f87f28563fc8064725230a0884b5a85d42f7037
kaludematto <kalu.dematto@gmail.com>
2025-01-23 22:46:32 -0300

    update

---
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -0,0 +1,28 @@
+import { AnswerRepository } from '../repositories/answers-repository'
+
+interface DeleteAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+}
+
+interface DeleteAnswerUseCaseResponse {}
+
+export class DeleteAnswerUseCase {
+  constructor(private answersRepository: AnswerRepository) {}
+
+  async execute({
+    answerId,
+  }: DeleteAnswerUseCaseRequest): Promise<DeleteAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    await this.answersRepository.delete(answer)
+
+    return {
+      answer,
+    }
+  }
+}
--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -8,20 +8,18 @@ interface DeleteQuestionUseCaseResponse {}
 
 export class DeleteQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
-
   async execute({
     questionId,
+    authorId,
   }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
     const question = await this.questionsRepository.findById(questionId)
-
     if (!question) {
       throw new Error('Question not found.')
     }
-
-    await this.questionsRepository.delete(question)
-
-    return {
-      question,
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
     }
+    await this.questionsRepository.delete(question)
+    return {}
   }
 }
