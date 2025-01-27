d9376d7700eea7c6d31329cd18207acbf1b87b47
kaludematto <kalu.dematto@gmail.com>
2025-01-27 12:53:15 -0300

    create comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,43 @@
+import { QuestionsRepository } from '../repositories/question-repository'
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comment-repository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionsRepository: QuestionsRepository,
+    private questionComments: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionsRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityID(authorId),
+      questionId: new UniqueEntityID(questionId),
+      content,
+    })
+
+    await this.questionComments.create(questionComment)
+
+    return { questionComment }
+  }
+}
