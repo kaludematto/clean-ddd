d496478e4d791274a18e92baa84bda4f18c1e4a7
kaludematto <kalu.dematto@gmail.com>
2025-02-06 10:28:22 -0300

    fix all unit tests

--- src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -12,28 +12,33 @@ describe('Fetch Answer Comments', () => {
     inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
     sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository)
   })
+
   it('should be able to fetch answer comments', async () => {
     await inMemoryAnswerCommentsRepository.create(
       makeAnswerComment({
         answerId: new UniqueEntityID('answer-1'),
       }),
     )
+
     await inMemoryAnswerCommentsRepository.create(
       makeAnswerComment({
         answerId: new UniqueEntityID('answer-1'),
       }),
     )
+
     await inMemoryAnswerCommentsRepository.create(
       makeAnswerComment({
         answerId: new UniqueEntityID('answer-1'),
       }),
     )
-    const { answerComments } = await sut.execute({
+
+    const result = await sut.execute({
       answerId: 'answer-1',
       page: 1,
     })
-    expect(answerComments).toHaveLength(3)
+    expect(result.value?.answerComments).toHaveLength(3)
   })
+
   it('should be able to fetch paginated answer comments', async () => {
     for (let i = 1; i <= 22; i++) {
       await inMemoryAnswerCommentsRepository.create(
@@ -42,10 +47,10 @@ describe('Fetch Answer Comments', () => {
         }),
       )
     }
-    const { answerComments } = await sut.execute({
+    const result = await sut.execute({
       answerId: 'answer-1',
       page: 2,
     })
-    expect(answerComments).toHaveLength(2)
+    expect(result.value?.answerComments).toHaveLength(2)
   })
 })
